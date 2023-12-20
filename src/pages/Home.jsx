57fc9b45abb0683a7ffb6ad94443ffa625e059d2
PC-127 <atanu@matrixnmedia.com>
2023-12-20 20:41:49 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Main, Product, Footer } from "../components";
+
+// this product here is of the listing product section
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Navbar, Product } from "../components"
+
+// this product here is of the single product section
+const Products = () => {
+  return (
+    <>
+      <Navbar />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Products
